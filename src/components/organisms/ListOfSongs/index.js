2f46d7bff648b138c4b2d1a2ac544ac0edc14aa0
Diego <diegoandrestrom04@gmail.com>
2021-06-21 14:28:40 -0500

    feat: initial design favorites

--- src/components/organisms/ListOfSongs/index.js
+++ src/components/organisms/ListOfSongs/index.js
@@ -1,5 +1,7 @@
 import { Cover } from 'components/molecules/Cover'
+import { MdFavorite } from 'react-icons/md'
 import { Table, TableHead, TableRow, TableBody } from './style'
+import { colors } from 'consts/styles'
 
 export function ListOfSongs ({ songs }) {
   return (
@@ -26,7 +28,10 @@ export function ListOfSongs ({ songs }) {
             </td>
             <td className='album'>Album 1</td>
             <td className='date'>2002/02/02</td>
-            <td className='duration'>4 min</td>
+            <td className='duration'>
+              <MdFavorite color={colors.primary} width='2em' height='2em' />
+              <span style={{ marginLeft: '.5rem' }}>4 min</span>
+            </td>
           </TableRow>
         ))}
       </TableBody>
